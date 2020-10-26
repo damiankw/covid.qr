49f5c1be31afd4e80ba064efff5ff2dea7600b9d
damian <damian@damian.id.au>
2020-10-26 17:40:25 +1100

    Created basic config.

---
+++ include/config.example.php
@@ -0,0 +1,8 @@
+<?php
+    $_CONFIG = array(
+        'db_hostname' => 'localhost',
+        'db_username' => 'covid_user',
+        'db_password' => 'covid_pass',
+        'db_database' => 'covid_qr'
+    );
+?>
